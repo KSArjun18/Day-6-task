6a881d157f23e3fcb99274933f4f969b6005434f
KSArjun18 <111218912+KSArjun18@users.noreply.github.com>
2022-08-25 15:43:22 +0530

    Add files via upload

---
+++ cricle.js
@@ -0,0 +1,21 @@
+class Cricle{
+    constructor(radius,color,area){
+        this.radius=radius;
+        this.color=color;
+        this.area=area;
+
+    }
+    getradius(){
+        return(`${this.radius}`)
+
+
+    }
+    getcolor(){
+        return(`${this.color}`)
+
+    }
+    getArea(){
+        return(`$this.area`)
+    }}
+     let C1= new Cricle(1.0,"red","200sqft");
+     console.log(C1);
