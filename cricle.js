f668e9a63eb77ea2046808de41b883fff2edb869
KSArjun18 <111218912+KSArjun18@users.noreply.github.com>
2022-08-27 11:03:40 +0530

    Update cricle.js

--- cricle.js
+++ cricle.js
@@ -1,21 +1,27 @@
-class Cricle{
-    constructor(radius,color,area){
-        this.radius=radius;
-        this.color=color;
-        this.area=area;
-
-    }
-    getradius(){
-        return(`${this.radius}`)
-
-
-    }
-    getcolor(){
-        return(`${this.color}`)
-
-    }
-    getArea(){
-        return(`$this.area`)
-    }}
-     let C1= new Cricle(1.0,"red","200sqft");
-     console.log(C1);
+class Circle{
+   constructor(radius, color){
+   this. radius=radius;
+   this.color=color;
+   }
+   get calc(){
+   console.log("The properties of the Circle are ");
+   console.log(this.circleradius());
+   console.log(this.circlecircumference());
+   console.log(this.circlearea());
+   }
+   circleradius() {
+   return 'The radius is ${this.radius}';
+   }
+   circlecircumference() {
+   return `The circumference is ${(this.radius*2*Math.PI).toFixed(2)}`;
+   }
+   circlearea(){
+   return `The area is ${(Math.pow(this.radius, 3)*4*Math. PI/3).toFixed(2)}`;
+   }
+   }
+   var rad1=new Circle (1.0,"red");
+   console.log(rad1);
+   console.log(rad1.calc);
+   var rad2=new Circle(5, "blue");
+   console.log(rad2);
+   console.log(rad2.calc);
